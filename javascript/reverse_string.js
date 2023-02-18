dd676ccab4f1159be7b8d13b10f3591398f4da8c
callix miller <calmill413@gmail.com>
2023-02-18 17:11:55 -0500

    javascript tests passing

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,5 @@
 function reverseString(str) {
-  // type your code here
+  return str.split("").reverse().join("")
 }
 
 if (require.main === module) {
@@ -11,9 +11,24 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("Expecting: 'yad'");
+  console.log("=>", reverseString("day"));
+
+  console.log("Expecting: 'yppah'");
+  console.log("=>", reverseString("happy"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+/*
+Create a function that accepts a string and returns the string text backwards.
+*/
 // And a written explanation of your solution
+/*
+reverseString accepts an argument of a string.
+  the string is changed into an array with spilt and the letters are spaced out from each other
+    the array values are reassembled backwards with the reverse value
+      the array is joined together to return the letters back into a string
+*/
